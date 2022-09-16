637a18cb649a132230de3acda863b7ae73a43c04
214G5A0512 <112165557+214G5A0512@users.noreply.github.com>
2022-09-16 15:58:00 +0530

    vamsi sample.c

---
+++ sample.c
@@ -0,0 +1,6 @@
+Name Of the Candidate: C.Vamsi
+   Roll No: 214g5a0512
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 16-09-2022
+   Date of Execution: 16-09-2022
+*/
